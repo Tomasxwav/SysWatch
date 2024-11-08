e0a06047dc45237b38ff6ad65b5883c8c2f6708e
Tomasxwav <tomas_everardo9@hotmail.com>
2024-11-08 13:51:14 -0600

    FIx: Error on update the state of servers in useScan (clean on disconnect)

--- src/hooks/useScan.tsx
+++ src/hooks/useScan.tsx
@@ -4,20 +4,20 @@ export const useScan = (isConnected: boolean) => {
   const [servers, setServers] = useState<string[] | null>([])
 
   useEffect(() => {
-    if (!isConnected) return // Solo ejecuta el efecto si isConnected es true
+    if (!isConnected) {
+      setServers([])
+      return
+    } // Solo ejecuta el efecto si isConnected es true
 
     const fetchServers = async () => {
       try {
         const foundServers = await window.electron.scanNetwork()
-        console.log(foundServers.length)
         if (foundServers.length === 0) {
           setServers(null)
           return
         } else {
           setServers(foundServers)
         }
-
-        setServers(foundServers)
       } catch (error) {
         console.error('Error al escanear la red:', error)
       }
@@ -25,5 +25,5 @@ export const useScan = (isConnected: boolean) => {
 
     fetchServers()
   }, [isConnected])
-  return servers ?? null
+  return servers
 }
