67dffd02b5528ada51271a6ad93289ded771377b
Tomasxwav <tomas_everardo9@hotmail.com>
2024-11-25 13:21:55 -0600

    Fix: Prevening send info on closed server

--- electron/sendInfo.js
+++ electron/sendInfo.js
@@ -3,28 +3,16 @@ import os from 'os'
 
 function sendHardware(hardware, server, port = 8080) {
   const socket = new net.Socket()
-  try {
-    socket.connect(port, server, () => {
-      socket.write(JSON.stringify(hardware))
-    })
-  } catch (error) {
-    console.error('Error al enviar la información:', error)
-  }
-
-  /* 
 
   socket.on('connect', () => {
-          socket.write(JSON.stringify(hardware))
-
+    socket.write(JSON.stringify(hardware))
   })
-
-
 
   socket.on('error', (err) => {
     socket.destroy()
   })
 
-  socket.connect(port, server) */
+  socket.connect(port, server)
 }
 
 export const sendInfo = (server, counter) => {
